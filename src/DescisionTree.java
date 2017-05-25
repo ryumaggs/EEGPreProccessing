0cdec3ce79c14fa635812afaa83ef3cf8fa7ea6c
ryumaggs <ryu@colgate.edu>
2017-05-25 16:36:31 -0400

    Added new class, channel, and redfined functions in DT

--- src/DescisionTree.java
+++ src/DescisionTree.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 /*
-	1.Takes a Complex[][] from a single channel where each wavelength is represented as a COLLUMN of this array of arrays
+	1.Takes a Complex[][] from a single channel where each wavelength is represented as a row of this array of arrays
 	2.Runs FFT on each wavelength, storing it in a new Complex[][] 
 	3.Picks two trials: (0,1)
 	4.Loop through the wavelengths of the remaining trials comparing it to the picked trials
@@ -10,67 +10,82 @@ import java.io.*;
 
 public class DescisionTree {
 	
-	private int num_freq;
-	private Complex[][] freq;
+	//
+	private int trial_type;
+	private Channel[] channels;
 	
-	//Used to condense trial data into an int (for trial type) and Complex[][]
-	public DescisionTree(int frequ, Complex[][] eeg_values){
-		num_freq = frequ;
-		for (int i = 0; i < eeg_values.length; i++){
-			Complex[] fftdata = fft(eeg_values[i]);
-			freq[i] = fftdata;
+	//Used to condense trial data into an int (for trial type) and channel data
+	//CHANGE THE PARAMETER, figure out what type of data it should take
+	public DescisionTree(int frequ, Complex[][] eeg_data){
+		trial_type = frequ;
+		for (int i = 0; i < channels.length; i++){
+			Channel temp = new Channel(eeg_data);
+			channels[i] = temp;
 		}
 	}
 	
-	public double compareWaveLength(int wave_num, DescisionTree base, DescisionTree trial){
-		double re_diff_sum = 0.0;
-		double im_diff_sum = 0.0;
-		
+	public int num_wlengths(Complex[][] channel){
+		return channel.length;
+	}
+	
+	public double compareWaveLength(Complex[][] base_fft, Complex[][] trial_fft){
+		/*
+		 Input: the wavelength you are looking at, the base DT, and the trial DT
+		 Output: returns the overall distance from the origin of the difference between the base and trial
+		 
+		 goes through the wavelengths of ONE channel in ONE trial and returns their difference distance in a
+		 double array
+		 */
+		int num_freq = base_fft.length;
+		double[] diff_dist = new double[num_freq];
 		//probably have to change this upper bound
-		for(int i = 0; i < base.freq.length; i++){
-			re_diff_sum += Math.abs((base.freq[i][wave_num]).re() - (trial.freq[i][wave_num]).re());
-			im_diff_sum += Math.abs((base.freq[i][wave_num]).im() - (trial.freq[i][wave_num]).im());
+		for (int i = 0; i < num_freq; i++){
+			double re_diff_sum = 0.0;
+			double im_diff_sum = 0.0;
+			for (int j = 0; j < base_fft[0].length; j++){
+				re_diff_sum += Math.abs(trial_fft[i][j].re() - (base_fft[i][j]).re());
+				im_diff_sum += Math.abs(trial_fft[i][j].im() - (base_fft[i][j]).im());
+			
+			}
 		}
 		
 		double complexMod = Math.sqrt(Math.pow(re_diff_sum, 2.0) + Math.pow(im_diff_sum, 2.0));
 		
 		return complexMod;
 	}
-	// taken from princeton
-    public static Complex[] fft(Complex[] x) {
-        int n = x.length;
+	
+	public int minIndex(double[] distances){
+		/*
+		 * Returns the index of the lowest value in a double array: this is the best wavelength
+		 * for a particular channel
+		 */
+		double min = distances[0];
+		int min_index = 0;
+		for (int i = 0; i < distances.length; i++){
+			if (distances[i] < min)
+				min_index = i;
+		}
+		
+		return min_index;
+	}
+	
+	public int[] bestWaves(DescisionTree base, DescisionTree[] trials){
+		/*
+		 * Goes through each channel in a trial and computes the best wavelength for each channel
+		 */
+		int[] best_waves = new int[trials.length];
+		for (int i = 0; i < trials.length; i++){
+			double[] wLengths = new double[trials.length];
+			for (int j = 0; j < (trials[i]).length; j++){
+				wLengths[j] = compareWaveLength(j,base,trials[i]);
+			}
+			
+		}
+	}
 
-        // base case
-        if (n == 1) return new Complex[] { x[0] };
-
-        // radix 2 Cooley-Tukey FFT
-        if (n % 2 != 0) { throw new RuntimeException("n is not a power of 2"); }
-
-        // fft of even terms
-        Complex[] even = new Complex[n/2];
-        for (int k = 0; k < n/2; k++) {
-            even[k] = x[2*k];
-        }
-        Complex[] q = fft(even);
-
-        // fft of odd terms
-        Complex[] odd  = even;  // reuse the array
-        for (int k = 0; k < n/2; k++) {
-            odd[k] = x[2*k + 1];
-        }
-        Complex[] r = fft(odd);
-
-        // combine
-        Complex[] y = new Complex[n];
-        for (int k = 0; k < n/2; k++) {
-            double kth = -2 * k * Math.PI / n;
-            Complex wk = new Complex(Math.cos(kth), Math.sin(kth));
-            y[k]       = q[k].plus(wk.times(r[k]));
-            y[k + n/2] = q[k].minus(wk.times(r[k]));
-        }
-        return y;
-    }
 	public static void main(String args[]){
+		int num_channels = 0;
+		
 		//nothing
 	}
 }
