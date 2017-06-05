6cc57a1456265732ba37be7a3b0092279187c07d
ryumaggs <ryu@colgate.edu>
2017-06-05 11:15:57 -0400

    Changed filter to no longer take base cases. Now takes a destination folder

--- src/filterGUI.java
+++ src/filterGUI.java
@@ -3,18 +3,16 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class filterGUI extends Frame implements ActionListener{
-	private Button base1;
-	private Button base2;
+
 	private Button remTrials;
 	private Button go;
+	private Button destination;
 
-	private String base1path;
-	private String base2path;
 	private String restTrialsDirPath; // save a directory
+	private String destinationPath;
 	
-	private TextField b1path = new TextField(10);
-	private TextField b2path = new TextField(10);
-	private TextField rpath = new TextField(10);
+	private TextField rpath;
+	private TextField dpath;
 
 	
 	JPanel panel;
@@ -22,9 +20,8 @@ public class filterGUI extends Frame implements ActionListener{
 	public filterGUI(){
 		/*
 		 * Set up the JFrame which should contain:
-		 * Base Trial 1 Button
-		 * Base Trial 2 Button
-		 * Shift select button for the rest of the trial data
+		 * button for the trial data selection
+		 * button for destination selection
 		 */
 
 		
@@ -37,26 +34,21 @@ public class filterGUI extends Frame implements ActionListener{
 		//Set up the JPanel and buttons
 		panel = new JPanel();
 		go = new Button("Run Mu Filter");
-		base1 = new Button("Base1");
-		base2 = new Button("Base2");
 		remTrials = new Button("Remaining Trials");
-		
-		b1path = new TextField("Path for base 1");
-		b2path = new TextField("Path for base 2");
+		destination = new Button("Destination Folder");
+
 		rpath = new TextField("Path for directory containing all trials");
+		dpath = new TextField("Path for destination directory");
 		
 		go.addActionListener(this);
-		base1.addActionListener(this);
-		base2.addActionListener(this);
 		remTrials.addActionListener(this);
+		destination.addActionListener(this);
 
 		//Add everything to the JFrame
-		panel.add(b1path);
-		panel.add(b2path);
 		panel.add(rpath);
+		panel.add(dpath);
 		panel.add(go);
-		panel.add(base1);
-		panel.add(base2);
+		panel.add(destination);
 		panel.add(remTrials);
 		frame.add(panel);
 		
@@ -68,30 +60,26 @@ public class filterGUI extends Frame implements ActionListener{
 
 	public void actionPerformed(ActionEvent e){
 		/*
-		 * Open up directory selector by pressing any of the 3 buttons.
+		 * Open up directory selector by pressing remTrials or Destination
 		 * Navigate to the file or directory
 		 * Press Save
 		 * And it saves the path into a String
 		 */
 		Object holder = e.getSource();
 
-		if (holder == base1 | holder == base2 | holder == remTrials){
+		if (holder == remTrials || holder == destination){
 			JFileChooser chose = new JFileChooser();
 			chose.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		
 			int result = chose.showSaveDialog(this);
+			
 			if (result == JFileChooser.APPROVE_OPTION){
-				if (e.getSource() == base1){
-					b1path.setText(chose.getSelectedFile().getAbsolutePath());
-					base1path = b1path.getText();
-				}
-				else if (e.getSource() == base2){
-					b2path.setText(chose.getSelectedFile().getAbsolutePath());
-					base2path = b2path.getText();
-				}
-				else if (e.getSource() == remTrials){
+				if (holder == remTrials){
 					rpath.setText(chose.getSelectedFile().getAbsolutePath());
 					restTrialsDirPath = rpath.getText();
+				}
+				else if (holder == destination){
+					dpath.setText(chose.getSelectedFile().getAbsolutePath());
+					destinationPath = dpath.getText();
 				}
 			}
 			else if (result == JFileChooser.CANCEL_OPTION){
@@ -99,8 +87,8 @@ public class filterGUI extends Frame implements ActionListener{
 			}
 		}
 		else if (holder == go){
-			if (base1path != null)
-				new RawFilter(base1path,8,16);
+			if (restTrialsDirPath != null)
+				new RawFilter(restTrialsDirPath,8,16);
 			else
 				System.out.println("no filepath");
 		}
