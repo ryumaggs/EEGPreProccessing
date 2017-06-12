405e2f2a8a7b4d91268de51b0c78f1eee420be6c
ryumaggs <ryu@colgate.edu>
2017-06-12 16:49:15 -0400

    added new variable to hold model name for later prediction

--- src/train_and_predict.java
+++ src/train_and_predict.java
@@ -8,14 +8,15 @@ import java.io.*;
 public class train_and_predict extends Frame implements ActionListener{
 	JFrame frame;
 	JPanel panel;
-	String folder_path;
-	String svm_dpath;
+	String training_file_path;
+	String svm_dir_path;
+	String model_path;
 	Button set_path;
 	Button svm_path;
 	Button train;
 	
 	public train_and_predict(){
-		folder_path = "";
+		training_file_path = "";
 		frame = new JFrame("train and predict");
 		frame.setSize(400,200);
 		frame.setLocationRelativeTo(null);
@@ -52,19 +53,21 @@ public class train_and_predict extends Frame implements ActionListener{
 			int result = chose.showSaveDialog(this);
 			
 			if (result == JFileChooser.APPROVE_OPTION){
-				if (holder == set_path)
-					folder_path = add_quotes(chose.getSelectedFile().getAbsolutePath());
+				if (holder == set_path){
+					model_path = chose.getSelectedFile().getAbsolutePath();
+					training_file_path = add_quotes(chose.getSelectedFile().getAbsolutePath());
+				}
 				else if (holder == svm_path)
-					svm_dpath = add_quotes(chose.getSelectedFile().getAbsolutePath());
+					svm_dir_path = add_quotes(chose.getSelectedFile().getAbsolutePath());
 			}
 		}
-		System.out.println(folder_path + " : " + svm_dpath);
+		System.out.println(training_file_path + " : " + svm_dir_path);
 		if (holder == train){
 			if (svm_path.equals("") || set_path.equals(""))
 				System.out.print("error: paths are not both set");
 			else{
 				try{
-				ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "set classpath = \"C:\\Users;C;\\Users\\SVMjava\\libsvm.jar\" && cd "+svm_dpath+" && java -classpath libsvm.jar svm_train " +folder_path);
+				ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "set classpath = \"C:\\Users;C;\\Users\\SVMjava\\libsvm.jar\" && cd "+svm_dir_path+" && java -classpath libsvm.jar svm_train " +training_file_path);
 		        builder.redirectErrorStream(true);
 		        Process p = builder.start();
 		        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -74,12 +77,12 @@ public class train_and_predict extends Frame implements ActionListener{
 		            if (line == null) { break; }
 		            System.out.println(line);
 		        }
-			}catch(IOException p){
-				System.out.print("erororororor");
+		        model_path = add_quotes(model_path + ".model");
+		        System.out.println("model path should be: " + model_path);
+			}catch(IOException p){System.out.print("erororororor");}
 			}
 		}
 	}
-}
 	public String add_quotes(String p){
 		String w = "\""+p+"\"";
 		return w;
