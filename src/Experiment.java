b40ac3bbbfabfa8703298c6ad1b30f13f1d983b9
ryumaggs <ryu@colgate.edu>
2017-06-19 11:37:20 -0400

    fixed slideshow for experiment so photos will now appear and cycle

--- src/Experiment.java
+++ src/Experiment.java
@@ -1,19 +1,18 @@
 import java.awt.BorderLayout;
 import java.io.*;
-//import javax.comm.*;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-//import java.util.concurrent.TimeUnit;
 
 
 public class Experiment{
 	private static ImageIcon[] frames;
 	private static JFrame exp2;
-	private static JPanel[] imageDir;
+	private static JPanel panel;
+	private static JLabel label;
 	
-	public static void beginExp(String photos){
+	public Experiment(String photos){
 		try{
 		loadImages(photos);
 		}catch(IOException e){e.printStackTrace();}
@@ -21,54 +20,48 @@ public class Experiment{
 		exp2 = new JFrame("asijaisj");
 		exp2.setSize(1000, 1000);
 		exp2.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		//exp2.setUndecorated(true);
+		exp2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel = new JPanel();
+		label = new JLabel();
+		panel.add(label,BorderLayout.CENTER);
+		exp2.add(panel,BorderLayout.CENTER);
 		exp2.setVisible(true);
-		int current = 0;
-		int stopped = 0;
-		while(true){
-			current = nextImage(current);
-			exp2.repaint();
-			stopped++;
-			if (stopped == 10)
-				break;
-			try{
-				Thread.sleep(1000);
-			}catch(InterruptedException ep){System.out.println("erorro");}
-		}
 	}
 	
 	public static void loadImages(String photo)throws IOException{
 		File dir = new File(photo);
 		File[] directoryListing = dir.listFiles();
 		frames = new ImageIcon[directoryListing.length];
-		System.out.println(frames.length + " is the length of frames");;
-		imageDir = new JPanel[frames.length];
-		System.out.println(imageDir.length + " is the length of imageDir");
-		
-		JLabel temp;
-		for(int i = 0; i < directoryListing.length; i++){
-			System.out.println("Currently looking at: " + directoryListing[i].getAbsolutePath());
-			frames[i] = new ImageIcon(directoryListing[i].getAbsolutePath());
-			temp = new JLabel("",frames[i],JLabel.CENTER);
-			imageDir[i] = new JPanel();
-			imageDir[i].add(temp);
-			exp2.add(imageDir[i]);
+		for (int i = 0; i < directoryListing.length; i++){
+			frames[i] = new ImageIcon(directoryListing[i].getPath());
 		}
 	}
 	
 	public static int nextImage(int cur){
-		//exp2.removeAll();
-		//exp2.revalidate();
 		System.out.println("looking at image nmber: " + cur);
 		if (cur == frames.length-1)
 			cur = 0;
 		else 
 			cur +=1;
-		
-		imageDir[cur].setVisible(true);
-		
+	
+		label.setIcon(frames[cur]);
 		return cur;
 		
+	}
+	
+	public static void main(String args[]){
+		new Experiment("C:\\Users\\Ryan Yu\\workspace\\ImportantFreq\\src\\ExperimentPhotos\\HC");
+		int current = 0;
+		int stopped = 0;
+		while(true){
+			current = nextImage(current);
+			stopped++;
+			if (stopped == 10)
+				break;
+			try{
+				Thread.sleep(4000);
+			}catch(InterruptedException ep){System.out.println("erorro");}
+		}
 	}
 	
 }
