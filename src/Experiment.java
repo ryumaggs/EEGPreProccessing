5c0c8c99729af525412660c65b14635afde559dc
ryumaggs <ryu@colgate.edu>
2017-06-22 10:52:58 -0400

    slideshow of images with blanks inbetween is now funcional (still does not write anything to files yet)

--- src/Experiment.java
+++ src/Experiment.java
@@ -1,23 +1,28 @@
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.swing.JLabel;
 
 
-public class Experiment{
-	private static ImageIcon[] frames;
-	private static JFrame exp2;
-	private static JPanel panel;
-	private static JLabel label;
+public class Experiment implements ActionListener,Runnable{
+	private ImageIcon[] frames;
+	private JFrame exp2;
+	private JPanel panel;
+	private JLabel label;
+	private Button begin;
 	
+	public Experiment(){
+		//blank creator for menu
+	}
 	public Experiment(String photos){
 		try{
 		loadImages(photos);
 		}catch(IOException e){e.printStackTrace();}
-		
-		exp2 = new JFrame("asijaisj");
+		exp2 = new JFrame("Experiment");
 		exp2.setSize(1000, 1000);
 		exp2.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		exp2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,7 +33,31 @@ public class Experiment{
 		exp2.setVisible(true);
 	}
 	
-	public static void loadImages(String photo)throws IOException{
+	public void scroll_images(){
+		//DataStreaming teststream = new DataStreaming();
+		//teststream.setupconnection("Newtester.txt");
+		int current = 0;
+		int stopped = 0;
+		label.setIcon(null);
+		try{
+		Thread.sleep(2000);
+		}catch(InterruptedException ep){ep.printStackTrace();}
+		while(true){
+			//System.out.println("entered the experiment loop");
+			current = nextImage(current);
+			stopped++;
+			if (stopped == 10)
+				break;
+			try{
+				Thread.sleep(4000);
+			}catch(InterruptedException ep){System.out.println("erorro");}
+			label.setIcon(null);
+			try{
+			Thread.sleep(2000);
+			}catch(InterruptedException ep){ep.printStackTrace();}
+		}
+	}
+	public void loadImages(String photo)throws IOException{
 		File dir = new File(photo);
 		File[] directoryListing = dir.listFiles();
 		frames = new ImageIcon[directoryListing.length];
@@ -37,8 +66,12 @@ public class Experiment{
 		}
 	}
 	
-	public static int nextImage(int cur){
-		System.out.println("looking at image nmber: " + cur);
+	public void run(){
+		scroll_images();
+	}
+	
+	public int nextImage(int cur){
+		//System.out.println("looking at image nmber: " + cur);
 		if (cur == frames.length-1)
 			cur = 0;
 		else 
@@ -49,22 +82,32 @@ public class Experiment{
 		
 	}
 	
-	public static void main(String args[]){
-		new Experiment("C:\\Users\\Ryan Yu\\workspace\\ImportantFreq\\src\\ExperimentPhotos\\HC");
-		int current = 0;
-		int stopped = 0;
-		while(true){
-			current = nextImage(current);
-			stopped++;
-			if (stopped == 10)
-				break;
-			try{
-				Thread.sleep(4000);
-			}catch(InterruptedException ep){System.out.println("erorro");}
+	public void actionPerformed(ActionEvent e){
+		Object holder = e.getSource();
+		if (holder == begin){
+			Experiment images = new Experiment("C:\\Users\\Ryan Yu\\workspace\\ImportantFreq\\src\\ExperimentPhotos\\HC");
+			Thread t = new Thread(images);
+			t.start();
+			(SwingUtilities.windowForComponent(begin)).dispose();
 		}
 	}
 	
+	public void load_menu(){
+		exp2 = new JFrame("Experiment");
+		exp2.setSize(1000, 1000);
+		exp2.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		exp2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		begin = new Button("Begin");
+		begin.addActionListener(this);
+		panel = new JPanel();
+		panel.add(begin);
+		exp2.add(panel,BorderLayout.CENTER);
+		exp2.setVisible(true);
+	}
+	
+	public static void main(String args[]){
+		Experiment menu = new Experiment();
+		menu.load_menu();
+	}
+	
 }
-
-
-
