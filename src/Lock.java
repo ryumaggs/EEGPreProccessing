8e77b2605ed53ed687da3a5684c466223e6fa083
ryumaggs <ryu@colgate.edu>
2017-06-29 16:17:52 -0400

    lock class to ensure that picture change and file write marking picture change happen concurrently

---
+++ src/Lock.java
@@ -0,0 +1,22 @@
+
+public class Lock {
+	private boolean isLocked = false;
+	
+	public synchronized void lock() throws InterruptedException{
+		while(isLocked){
+			wait();
+		}
+		isLocked = true;
+	}
+	
+	public synchronized void unlock(){
+		isLocked = false;
+		notify();
+	}
+	
+	public synchronized void check_lock() throws InterruptedException{
+		while(isLocked){
+			wait();
+		}
+	}
+}
