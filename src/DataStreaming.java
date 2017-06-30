5aa530f315e6e474c6cd5052664d40295e3c1ada
Jack <qianniao888@yahoo.com>
2017-06-30 16:58:30 -0400

    datastreaming comments

--- src/DataStreaming.java
+++ src/DataStreaming.java
@@ -29,24 +29,24 @@ public class DataStreaming implements SerialPortEventListener{
 	private static final double accelscale = .002/16;
 	private static DecimalFormat format2 = new DecimalFormat(".##");
 	
-	private int linecounter;
-	private byte[] backbuffer;
+	private int linecounter; 			//used for counting the first 5 lines of OPENBCI initialization data
+	private byte[] backbuffer;			//store data that can still be used as a result of detecting corrupted packets
 	private boolean usebackbuffer;
 	
+	//Initialize connections to the ComPort and set up  Buffered Streams to read and write to OPENBCI
 	public void setupconnection(String filename){
 		lock = Experiment.get_lock();
 		linecounter = 0;
-		
+		usebackbuffer = false;
+		String portName = "COM4";
 		Filename = filename;
+		
 		try{
 			filewriter = new BufferedWriter(new FileWriter(Filename));
 		}
 		catch (IOException e){
 			e.printStackTrace();
 		}
-		usebackbuffer = false;
-		
-		String portName = "COM4";
 		
 	    CommPortIdentifier portId = null;
 		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
@@ -76,7 +76,6 @@ public class DataStreaming implements SerialPortEventListener{
 
 			// open the streams
 			input = new BufferedInputStream(serialPort.getInputStream(),65536);
-			//input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
 			output = serialPort.getOutputStream();
 			
 			// add event listeners
@@ -91,107 +90,100 @@ public class DataStreaming implements SerialPortEventListener{
 	public synchronized void serialEvent(SerialPortEvent oEvent) {
 			try {
 				while(input.available() >= 33){
-				byte[] inputStrand = new byte[33];
-				int c;
-				if(usebackbuffer){
-					c = backbuffer[0];
-				}
-				else{
-					c = input.read();
-				}
-				//Read the first 4 lines of signal (OPENBCI header)
-				if(isalpha(c) && linecounter <4){
-					linecounter ++;
-					String inputLine= ((char) c) + linereader();
-					write2file(inputLine, false);
-				}
-				//Read the 5th line of signal (OPENBCI header "$$$")
-				else if(c == '$' && linecounter == 4){
-					linecounter ++;
-					input.read();
-					input.read();
-				}
-				else{
-					if(c == (byte) 0xA0){
-						inputStrand[0] = (byte) c;
-						input.read(inputStrand, 1, 32);
-						usebackbuffer = false;
-						backbuffer = null;
+					byte[] inputStrand = new byte[33];
+					int c;
+					if(usebackbuffer){
+						c = backbuffer[0];
 					}
 					else{
-						if(usebackbuffer){
-							int bblen = backbuffer.length;
-							System.arraycopy(backbuffer, 0, inputStrand, 0, bblen);
-							inputStrand[bblen] = (byte) c;
-							if(bblen < 32){
-								input.read(inputStrand, bblen+1, 32-bblen);
-							}
-						}
-						else{
+						c = input.read();
+					}
+					//Read the first 4 lines of OPENBCI initialization signal
+					if(isalpha(c) && linecounter <4){
+						linecounter ++;
+						String inputLine= ((char) c) + charlinereader();
+						write2file(inputLine, false);
+					}
+					//Read the datastreaming initialization signal "$$$" (5th line)
+					else if(c == '$' && linecounter == 4){
+						linecounter ++;
+						input.read();
+						input.read();
+					}
+					//Read a packet (33 bytes) of eeg data signal
+					else{
+						//If the header is the first character, then read the entire packet
+						if(c == (byte) 0xA0){
 							inputStrand[0] = (byte) c;
 							input.read(inputStrand, 1, 32);
+							usebackbuffer = false;
+							backbuffer = null;
 						}
-					}
-					if(handleconsistency(inputStrand)){
+						else{
+							//The packet header for this current packet is actually in a backbuffer
+							//generated as a result of data lost from the previous packet
+							if(usebackbuffer){
+								int bblen = backbuffer.length;
+								System.arraycopy(backbuffer, 0, inputStrand, 0, bblen);
+								inputStrand[bblen] = (byte) c;
+								if(bblen < 32){
+									input.read(inputStrand, bblen+1, 32-bblen);
+								}
+							}
+							//Read 33bytes even though the current packet is corrupted
+							else{
+								inputStrand[0] = (byte) c;
+								input.read(inputStrand, 1, 32);
+							}
+						}
+						//If packet is consistent, then process the packet and write it to a file
+						if(handleconsistency(inputStrand)){
+							double[] data = sampleparser(inputStrand);
+							String sdata = data2String(data) + '\n';
+							lock.check_lock();
+							write2file(sdata, false);
+						}
 						
-						double[] data = sampleparser(inputStrand);
-						String sdata = data2String(data) + '\n';
-						lock.check_lock();
-						write2file(sdata, false);
+						else{
+							System.out.println("lost packet");
+							//write2file("##################LOST PACKET###################", true);
 						}
-					else{
-						System.out.println("lost packet");
-						//write2file("##################LOST PACKET###################", true);
-					}
 					}
 				}
-				} catch (Exception e) {
+			} catch (Exception e) {
 				System.err.println(e.toString());
-			}
 		}
+	}
 	
 	/*
 	 * Check to see that the packet of data (33 bytes) is of consistent format (a header of 0xA0 and a footer between 0xC0-0xC6)
-	 * Returns: 0 if data is consistent; 1 if missing a header; 2 if missing a footer
 	 */
 	private boolean handleconsistency(byte[] data){
 		int lastidx = data.length -1;
 		int headerint = -1;
-		//if the header and footer exist in the correct position, return true
+		//if the header and footer exist in the correct position, then the packet is consistent, so return true
 		if(data[0] == (byte) 0xA0 && data[lastidx] >= (byte) 0xC0 && data[lastidx] <= (byte) 0xC6){
 			usebackbuffer = false;
 			backbuffer = null;
 			return true;
 		}
-		//if the first element is not a header, then find the index of the next header and append the remainder of the array to the backbuffer
+		//find the last index (not including 0) at which the header can be found
 		else{
-//			if(data[0] != (byte) 0xA0){
 				for(int i= 1; i<data.length; i++){
 					if(data[i] == (byte) 0xA0){
 						headerint = i;
-						//break;
 					}
 				}
-//			}
-//			else{
-//				for(int i= 1; i<data.length; i++){
-//					if(data[i] == (byte) 0xA0){
-//						headerint = i;
-//						break;
-//					}
-//					if(data[i] >= (byte) 0xC0 && data[i] <= (byte) 0xC6){
-//						headerint = i+1;
-//						break;
-//					}
-//				}
-//			}
 		}
+		//If there exist another header, then save the remainder of the packet starting from
+		//the header to a global backbuffer
 		if(headerint != -1 && headerint < data.length){
 			usebackbuffer = true;
 			int buffsize = data.length - headerint;
 			backbuffer = new byte[buffsize];
 			System.arraycopy(data, headerint, backbuffer, 0, buffsize);
 		}
+		//If there are no other header, then throw away the entire packet
 		else{
 			backbuffer = null;
 			usebackbuffer = false;
@@ -200,6 +192,7 @@ public class DataStreaming implements SerialPortEventListener{
 	}
 	
 	
+	//Check to see whether an int is an Alphabetical character
 	private boolean isalpha(int c){
 		if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
 			return true;
@@ -208,7 +201,9 @@ public class DataStreaming implements SerialPortEventListener{
 	}
 	
 	//ReadLine function for BufferedOutputStream
-	private String linereader(){
+	//Only works for reading lines that begin with an Alphabetical character
+	//Returns the line read or an empty string
+	private String charlinereader(){
 		String line = "";
 		try{
 			int curchar = input.read();
@@ -224,13 +219,14 @@ public class DataStreaming implements SerialPortEventListener{
 		return line;
 	}
 
-	//Parse a line of data into the follow components:
+	//Parse a line of raw OPENCBCI data into the follow components:
 	//samplenumber, eeg data, acceleration data
+	//Returns a double array of parsed data for one line of raw OPENBCI data
 	private double[] sampleparser(byte[] sample){
 		double[] data = new double[12];
-		//data[0] = Character.getNumericValue(sample[1]);
 		long l = sample[1] & 0xFFL;
-		data[0] = (int) l;
+		data[0] = (int) l;	//samplenumber
+		//eeg data
 		data[1] = interpret24bitAsInt32(bytesubarray(sample,2,4)) * eegscale;
 		data[2] = interpret24bitAsInt32(bytesubarray(sample,5,7)) * eegscale;
 		data[3] = interpret24bitAsInt32(bytesubarray(sample,8,10)) * eegscale;
@@ -239,11 +235,8 @@ public class DataStreaming implements SerialPortEventListener{
 		data[6] = interpret24bitAsInt32(bytesubarray(sample,17,19)) * eegscale;
 		data[7] = interpret24bitAsInt32(bytesubarray(sample,20,22)) * eegscale;
 		data[8] = interpret24bitAsInt32(bytesubarray(sample,23,25)) * eegscale;
-//		for(int eeg=0; eeg < 8; eeg++){
-//			int basecount = 2 + (3*eeg);
-//			System.out.print(basecount);
-//			data[eeg+1] = interpret24bitAsInt32(bytesubarray(sample,basecount, basecount+2)) * eegscale;
-//		}
+		
+		//acceleration data
 		for(int accel= 0; accel < 3; accel++){
 			int basecount = 26 + (2*accel);
 			byte[] unexpandedint = new byte[2];
@@ -254,7 +247,7 @@ public class DataStreaming implements SerialPortEventListener{
 		return data;
 	}
 	
-	//Retrieve a subarray of a byte array
+	//Returns a subarray (byte) of a byte array (start and end are inclusive)
 	private byte[] bytesubarray(byte[] arr, int start, int end){
 		byte[] subarr = new byte[end-start+1];
 		for(int i=0; i<subarr.length; i++){
@@ -263,7 +256,9 @@ public class DataStreaming implements SerialPortEventListener{
 		return subarr;
 	}
 	
+	
 	//Format a byte array to printable data for visualization
+	//Return a string of formatted eeg data
 	private String data2String(double[] data){
 		String dString = "";
 		for (int i=0; i < data.length; i++){
@@ -272,10 +267,9 @@ public class DataStreaming implements SerialPortEventListener{
 		return dString;
 	}
 	
-	//'nline' refers to if you NEED a newline character (aka don't have one in the string)
+	//Write data to file (set 'nline' to true to insert a newline character)
 	public void write2file(String data, Boolean nline){
 		try{
-			//System.out.println("uncloekd from write2file");
 			filewriter.write(data);
 			if(nline == true){
 				filewriter.newLine();
@@ -287,6 +281,7 @@ public class DataStreaming implements SerialPortEventListener{
 		}
 	}
 	
+	//Stop Streaming and Close Streaming Port
 	public void endstream(){
 		try{ 
 			output.write('s'); 
@@ -298,7 +293,7 @@ public class DataStreaming implements SerialPortEventListener{
 			}
 	}
 	
-	//OPENBCI function to convert 24bit eeg data to 32bit int
+	//OPENBCI function to convert 24bit OPENBCI eeg data to readable 32bit int
 	int interpret24bitAsInt32(byte[] byteArray) {     
 	    int newInt = (  
 	     ((0xFF & byteArray[0]) << 16) |  
@@ -313,7 +308,7 @@ public class DataStreaming implements SerialPortEventListener{
 	    return newInt;  
 	}  
 	
-	//OPENBCI fucntion to convert 16bit acceleration data to 32bit int
+	//OPENBCI function to convert 16bit OPENBCI acceleration data to readable 32bit int
 	int interpret16bitAsInt32(byte[] byteArray) {
 	    int newInt = (
 	      ((0xFF & byteArray[0]) << 8) |
